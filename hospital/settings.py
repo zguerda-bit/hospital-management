23134bea4f6a58d9286d3ad19fff6c012a5b31b3
zguerda-bit <z.guerda@esi-sba.dz>
2026-06-01 20:31:17 +0100

    Updated settings and latest project changes

--- hospital/settings.py
+++ hospital/settings.py
@@ -175,12 +175,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_PORT = 587
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'your email'
-EMAIL_HOST_PASSWORD = 'your email host password'
+EMAIL_HOST_USER = 'zinaguerda16@gmail.com'
+EMAIL_HOST_PASSWORD = 'zrxwbgiwekoujdes'
 
 AUTH_USER_MODEL = 'users.Users'
 DEFAULT_CHARSET = "utf-8"
